9647a3dc0c135b8b7f1002bc8af4d7c631635451
ArmyaAli <ali.umar.work@gmail.com>
2021-03-07 19:18:28 -0500

    cleaning comments from example code

--- client.c
+++ client.c
@@ -4,44 +4,42 @@
 
 int main(void)
 {
-    int sock; /* Socket descriptor */
-    int recv_size; // size of reciving buffer
-    struct sockaddr_in echoServAddr; /* Echo server address */
-    unsigned short serverPort;     // server port
-    char* servIP;                    /* Server IP address (dotted quad) */
-    int messageLen;                  /* Length of string to echo */
-    WSADATA wsaData;                 /* Structure for WinSock setup communication */
+    int recv_size = 0;
+    struct sockaddr_in echoServAddr;
+    SOCKET sock;
+    unsigned short serverPort;
+    char* servIP;
+    int messageLen;
+    WSADATA wsaData;
     char server_reply[2000];
     char* message;
-    servIP       = "127.0.0.1"; /* First arg: server IP address (dotted quad) */
-    serverPort = 8888;      /* Use given port, if any */
+    servIP     = "127.0.0.1"; // loopback
+    serverPort = 8888;
 
-    if (WSAStartup(MAKEWORD(2, 0), &wsaData) != 0) /* Load Winsock 2.0 DLL */
+    if (WSAStartup(MAKEWORD(2, 0), &wsaData) != 0)
     {
         fprintf(stderr, "WSAStartup() failed");
         exit(1);
     }
 
-    /* Create a reliable, stream socket using TCP */
     if ((sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
     {
         printf("Crashed at socket created");
     }
 
-    /* Construct the server address structure */
-    memset(&echoServAddr, 0, sizeof(echoServAddr)); /* Zero out structure */
+    memset(&echoServAddr, 0, sizeof(echoServAddr));
 
-    echoServAddr.sin_family      = AF_INET;             /* Internet address family */
-    echoServAddr.sin_addr.s_addr = inet_addr(servIP);   /* Server IP address */
-    echoServAddr.sin_port        = htons(serverPort); /* Server port */
+    echoServAddr.sin_family      = AF_INET;
+    echoServAddr.sin_addr.s_addr = inet_addr(servIP);
+    echoServAddr.sin_port        = htons(serverPort);
 
-    /* Establish the connection to the echo server */
+    /* establish connection */
     if (connect(sock, (struct sockaddr*)&echoServAddr, sizeof(echoServAddr)) < 0)
     {
         printf("Crashed at connect\n");
         exit(1);
     }
-    
+
     /* Send the string, including the null terminator, to the server */
     if (send(sock, message, messageLen, 0) != messageLen)
     {
